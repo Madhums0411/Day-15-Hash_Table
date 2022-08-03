7e9d96fab832d9659a2abe2afef8c813daaba35a
madhums0411 <madhums41175@gmail.com>
2022-08-03 23:07:17 +0530

    Find Frequency of large Paragraph

--- HashTable/HashTable/Program.cs
+++ HashTable/HashTable/Program.cs
@@ -20,5 +20,23 @@ class Program
         }
         int frequency = LinkedHashMap.Get("to");
         Console.WriteLine(frequency);
+
+        string Paragraph = "“Paranoids are not paranoid because they are " +
+            "paranoid but because they keep putting themselves deliberately into" +
+            " paranoid avoidable situations";
+        string[] letters = Paragraph.ToLower().Split(" ");
+
+        foreach (string word in letters)
+        {
+            int value = LinkedHashMap.Get(word);
+            if (value == default)
+            {
+                value = 1;
+            }
+            else value += 1;
+            LinkedHashMap.Add(word, value);
+        }
+        int frequency1 = LinkedHashMap.Get("paranoid");
+        Console.WriteLine(frequency1);
     }
 }
