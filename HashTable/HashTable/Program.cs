cc3a6c6bf65bc63d933ed2b21678bd68ec52e206
madhums0411 <madhums41175@gmail.com>
2022-08-03 23:00:10 +0530

    Find Frequency of words

--- HashTable/HashTable/Program.cs
+++ HashTable/HashTable/Program.cs
@@ -1,1 +1,24 @@
-﻿Console.WriteLine("Welcome to Hash Table");
+﻿using HashTable;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Welcome to Hash table");
+        LinkedHashMap<string, int> LinkedHashMap = new LinkedHashMap<string, int>(5);   
+        string sentence = "to be or not to be";
+        string[] words = sentence.ToLower().Split(" ");                                 
+        foreach (string word in words)                                                 
+        {
+            int value = LinkedHashMap.Get(word);
+            if (value == default)                                                      
+            {
+                value = 1;
+            }
+            else value += 1;                                                           
+            LinkedHashMap.Add(word, value);                                           
+        }
+        int frequency = LinkedHashMap.Get("to");
+        Console.WriteLine(frequency);
+    }
+}
